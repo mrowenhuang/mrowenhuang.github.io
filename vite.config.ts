aafeb666dd69fc5ffdb9ae64c1717354bef2a4c7
mrowenhuang <williamowenwijaya191204@gmail.com>
2026-03-21 12:09:09 +0700

    feat: initial commit

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import path from 'path'; // Import ini penting
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Menghubungkan @ ke folder src
+    },
+  },
+});
